80bac0d6850fa28ede0cfa5f80ae74d67b6cb789
Alwaleed Hamam <alwaleedhamam@MBPM2-FXCXCR72YF-HAMAM.local>
2024-04-24 09:24:20 -0400

    new commit

--- app/src/main/java/com/example/kotlinlearning/tasks/Task1.kt
+++ app/src/main/java/com/example/kotlinlearning/tasks/Task1.kt
@@ -16,30 +16,21 @@ Bank Cards
 
  Assuming card and date as input write a function to verify valid card numbers
   */
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 
 val validCompanyCodes = setOf("1121", "1111", "3796")
 
 fun verifyCard(bankCardNumbers: Long): Boolean {
     val cardString = bankCardNumbers.toString()
     val companyCode = cardString.substring(0, 4)
-    val expiryDateString = cardString.substring(cardString.length - 4)
+    val expiryDate = cardString.substring(cardString.length - 4)
 
     // Check if the first two digits represent a valid month (1-12)
-    val expiryMonth = expiryDateString.substring(0, 2).toIntOrNull()
+    val expiryMonth = expiryDate.substring(0, 2).toIntOrNull()
     if (expiryMonth == null || expiryMonth !in 1..12) {
         return false
     }
 
-    // Get the current date
-    val currentDate = LocalDate.now()
-
-    // Parse the expiry date string to LocalDate
-    val expiryDate = LocalDate.parse(expiryDateString, DateTimeFormatter.ofPattern("MMyy"))
-
-    // Check if the expiry date is in the future
-    return expiryDate.isAfter(currentDate) && validCompanyCodes.contains(companyCode)
+    return validCompanyCodes.contains(companyCode)
 }
 
 fun main() {
@@ -50,3 +41,5 @@ fun main() {
         println("Card is invalid")
     }
 }
+
+
